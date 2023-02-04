1fd2e2fcb6aebb6ebd1f6757bbbc33c27e3afd0f
Michal Boczula <michal.boczula0101@gmail.com>
2023-02-04 18:22:32 +0100

    Added layout for components

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductsEditComponent } from './products-edit/products-edit.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,11 @@ import { ProductsEditComponent } from './products-edit/products-edit.component';
     ProductsEditComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
+  ],
+  exports: [
+    ProductsListComponent
   ]
 })
 export class ProductsModule { }
