fa8311341f0d5677d612806898e78d290ceb0ba2
Michal Boczula <michal.boczula0101@gmail.com>
2023-02-05 23:13:15 +0100

    Added service and changed layout

--- src/app/products/products-list/products-list.component.ts
+++ src/app/products/products-list/products-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product-model';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
 
+  products!: Product[];
+  errorMessage!: string;
+
+  constructor(private productService: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: products => this.products = products,
+      error: err => this.errorMessage = err
+    });
+  }
 }
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { catchError, Observable, of, tap, throwError } from 'rxjs';
+import { Product } from '../product-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  private products: Product[] = [];
+  private productUrl: string = 'http://localhost:4201/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    if (!!this.products.length) {
+      return of(this.products);
+    }
+    else {
+      return this.httpClient.get<Product[]>(this.productUrl)
+        .pipe(
+          tap(data => console.log(JSON.stringify(data))),
+          tap(data => this.products = data),
+          catchError(this.handleError)
+        )
+    }
+  }
+
+  private handleError(err: any) {
+    const errorMessage = err.error.message;
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 }
